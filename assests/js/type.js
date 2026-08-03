3cfff64330672136d7905627da50052decd4cf6b
Md.Faysal Hasan <faysalhasanmd393@mail.com>
2026-08-03 23:08:31 +0600

    fix type method

---
+++ assests/js/type.js
@@ -0,0 +1,12 @@
+"use strict";
+const user = {
+    name: "faysal",
+    age: 25,
+    isAdmin: true,
+};
+const user2 = {
+    name: "alice",
+    age: 30,
+    isAdmin: false,
+};
+console.log(user2);
---
+++ assests/ts/type.ts
@@ -0,0 +1,18 @@
+type User = {
+  name: string;
+  age: number;
+  isAdmin: boolean;
+};
+
+const user: User = {
+  name: "faysal",
+  age: 25,
+  isAdmin: true,
+};
+
+const user2: User = {
+  name: "alice",
+  age: 30,
+  isAdmin: false,
+};
+console.log(user2);
