b1436cf9dc29c0fae669520ac53f1f60ca7d5375
Md.Faysal Hasan <faysalhasanmd393@mail.com>
2026-08-03 22:56:36 +0600

    fix replace method

---
+++ assests/js/second.js
@@ -0,0 +1,5 @@
+"use strict";
+let firstName;
+firstName = "faysal Hasan";
+let newName = firstName.replace("faysal Hasan", "faysal kadir");
+console.log(newName);
---
+++ assests/ts/second.ts
@@ -0,0 +1,4 @@
+let firstName: string;
+firstName = "faysal Hasan";
+let newName: string = firstName.replace("faysal Hasan", "faysal kadir");
+console.log(newName);
