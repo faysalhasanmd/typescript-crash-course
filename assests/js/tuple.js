379b7ccd074c5c95a5874c9d32e44eef9c73b9b0
Md.Faysal Hasan <faysalhasanmd393@mail.com>
2026-08-05 00:04:25 +0600

    solve tuple readonly property

--- assests/js/tuple.js
+++ assests/js/tuple.js
@@ -1,7 +1,8 @@
 "use strict";
 const tangailLocation = [10, 20];
 const tangailLocation2 = [10.25, 20.14];
-console.log(tangailLocation2[1]);
 const player1 = ["neymar", "brazil", 10];
 const player2 = ["ronaldo", "portugal", 7];
-console.log(player1[0]);
+const order1 = [10, 20];
+// order1.push(30); // This would cause an error since the tuple is readonly
+console.log(order1);
--- assests/ts/tuple.ts
+++ assests/ts/tuple.ts
@@ -1,9 +1,14 @@
 type point = [number, number];
 const tangailLocation: point = [10, 20];
 const tangailLocation2: point = [10.25, 20.14];
-console.log(tangailLocation2[1]);
+// console.log(tangailLocation2[1]);
 
 type player = [string, string, number];
 const player1: player = ["neymar", "brazil", 10];
 const player2: player = ["ronaldo", "portugal", 7];
-console.log(player1[0]);
+// console.log(player1[0]);
+
+type orderItem = readonly [number, number];
+const order1: orderItem = [10, 20];
+// order1.push(30); // This would cause an error since the tuple is readonly
+console.log(order1);
