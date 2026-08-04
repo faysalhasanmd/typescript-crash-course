099a34248a3369beef440ccd35a9436dc0817a49
Md.Faysal Hasan <faysalhasanmd393@mail.com>
2026-08-04 23:54:57 +0600

    solve tuple

---
+++ assests/js/tuple.js
@@ -0,0 +1,4 @@
+"use strict";
+const tangailLocation = [10, 20];
+const tangailLocation2 = [10.25, 20.14];
+console.log(tangailLocation2[1]);
---
+++ assests/ts/tuple.ts
@@ -0,0 +1,4 @@
+type point = [number, number];
+const tangailLocation: point = [10, 20];
+const tangailLocation2: point = [10.25, 20.14];
+console.log(tangailLocation2[1]);
