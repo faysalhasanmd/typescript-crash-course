5be1454cb16515fc1e956b3d99f7698b9f3a93dd
Md.Faysal Hasan <faysalhasanmd393@mail.com>
2026-08-04 23:59:46 +0600

    solve tuple with another example

--- assests/js/tuple.js
+++ assests/js/tuple.js
@@ -2,3 +2,6 @@
 const tangailLocation = [10, 20];
 const tangailLocation2 = [10.25, 20.14];
 console.log(tangailLocation2[1]);
+const player1 = ["neymar", "brazil", 10];
+const player2 = ["ronaldo", "portugal", 7];
+console.log(player1[0]);
--- assests/ts/tuple.ts
+++ assests/ts/tuple.ts
@@ -2,3 +2,8 @@ type point = [number, number];
 const tangailLocation: point = [10, 20];
 const tangailLocation2: point = [10.25, 20.14];
 console.log(tangailLocation2[1]);
+
+type player = [string, string, number];
+const player1: player = ["neymar", "brazil", 10];
+const player2: player = ["ronaldo", "portugal", 7];
+console.log(player1[0]);
