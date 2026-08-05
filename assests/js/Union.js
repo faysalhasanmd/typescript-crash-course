b7f276656d5ef3f082154917e7d8a4d155b324ae
Md.Faysal Hasan <faysalhasanmd393@mail.com>
2026-08-05 23:59:20 +0600

    solve union with rectangle and circle shape

--- assests/js/Union.js
+++ assests/js/Union.js
@@ -4,3 +4,13 @@ function printId(id) {
     console.log(`ID: ${id}`);
 }
 printId(1452);
+function calculateArea(shape) {
+    if ("radius" in shape) {
+        return Math.PI * shape.radius * shape.radius;
+    }
+    else {
+        return shape.width * shape.height;
+    }
+}
+console.log(calculateArea({ width: 10, height: 5 }));
+console.log(calculateArea({ radius: 5 }));
--- assests/ts/Union.ts
+++ assests/ts/Union.ts
@@ -6,3 +6,22 @@ function printId(id: ID) {
   console.log(`ID: ${id}`);
 }
 printId(1452);
+
+type rectangle = {
+  width: number;
+  height: number;
+};
+
+type circle = {
+  radius: number;
+};
+
+function calculateArea(shape: rectangle | circle): number {
+  if ("radius" in shape) {
+    return Math.PI * shape.radius * shape.radius;
+  } else {
+    return shape.width * shape.height;
+  }
+}
+console.log(calculateArea({ width: 10, height: 5 }));
+console.log(calculateArea({ radius: 5 }));
